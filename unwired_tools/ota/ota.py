4574d39f9428fa56ff276b78db92912e6f45b9ba
Manchenko Oleg <man4enkoos@gmail.com>
2018-12-26 19:18:44 +0300

    Add tools for OTA in progress

--- unwired_tools/ota/ota.py
+++ unwired_tools/ota/ota.py
@@ -6,15 +6,22 @@ ser = serial.Serial(
     parity=serial.PARITY_NONE,\
     stopbits=serial.STOPBITS_ONE,\
     bytesize=serial.EIGHTBITS,\
-    timeout=1)
+    timeout=0.1)
 
 print("connected to: " + ser.portstr)
 count = 1
 
 while True:
-    for line in ser.read():
+    result = ""
+    num = ser.read(300)    # read up to 300 bytes (timeout)
 
-        print(str(count) + str(': ')) #+ chr(line) )
-        count = count + 1
+    for i in num:
+        result += '0x{} '.format(i.encode('hex'))
+
+    print(result)
+    # for line in ser.read(300):
+
+    #     print(str(count) + str(': ')) #+ chr(line) )
+    #     count = count + 1
 
 ser.close()
